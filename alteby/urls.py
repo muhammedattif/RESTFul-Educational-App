f85ef7ea9bf0e9545bcc1b228e38a4ee607e911a
muhammedattif <muhamedattif@gmail.com>
2022-01-13 13:46:54 +0200

    test2

--- alteby/urls.py
+++ alteby/urls.py
@@ -20,7 +20,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.views.static import serve
-
+from django.conf import setings
 
 schema_view = get_schema_view(
    openapi.Info(
