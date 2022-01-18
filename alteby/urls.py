ee329f25af160d321334353c27d7a1ab74a6bb51
Muhammed Atif <muhamedattif@gmail.com>
2022-01-18 12:46:16 +0200

    add heroku conf

--- alteby/urls.py
+++ alteby/urls.py
@@ -19,8 +19,6 @@ from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from django.views.static import serve
-from django.conf import settings
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -68,7 +66,4 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-
-    path('media/<str:path>', serve,{'document_root':settings.MEDIA_ROOT}),
-    path('static/<str:path>', serve,{'document_root':settings.STATIC_ROOT}),
 ]
