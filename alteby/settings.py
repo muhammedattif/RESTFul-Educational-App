9ad400675902b7a1baa0d3d0632df2edcdf80e31
Muhammed Atif <muhamedattif@gmail.com>
2022-01-19 07:52:07 +0200

    update requirements

--- alteby/settings.py
+++ alteby/settings.py
@@ -184,3 +184,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5
 }
+
+import django_heroku
+django_heroku.settings(locals())
