92d01a3a1296981ce541f6c96ebb3ca67b971ff5
Muhammed Atif <muhamedattif@gmail.com>
2022-01-20 09:12:13 +0200

    add allowed hostes

--- alteby/settings.py
+++ alteby/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-pzxjj03z6a7+2xf-186h!7xy-mcq)hz3in-xz-06#a!1b)1^83
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['emtyaz-advisor.herokuapp.com']
 
 INTERNAL_IPS = [
     '127.0.0.1',
