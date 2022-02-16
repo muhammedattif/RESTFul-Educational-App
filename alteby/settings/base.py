679b2aab97d1cf2bb73e219ef3337f6f48ff85fd
muhammedatif <muhamedattif@gmail.com>
2022-02-16 23:57:14 +0200

    fix an issue in static files

--- alteby/settings/base.py
+++ alteby/settings/base.py
@@ -17,7 +17,7 @@ env = environ.Env()
 environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 AUTH_USER_MODEL = "users.User"
 
