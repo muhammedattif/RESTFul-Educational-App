81c69af878993482be625249dac0e7b981fc9f88
muhammedatif <muhamedattif@gmail.com>
2022-04-09 01:27:47 +0200

    Add whitenose

--- alteby/settings/base.py
+++ alteby/settings/base.py
@@ -85,6 +85,7 @@ ADMIN_REORDER = (
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
