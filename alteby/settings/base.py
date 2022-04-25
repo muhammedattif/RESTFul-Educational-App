976222a13fc5142c1854cb907b9c28d1a16021a1
muhammedatif <muhamedattif@gmail.com>
2022-04-25 12:41:32 +0200

    Update Environ

--- alteby/settings/base.py
+++ alteby/settings/base.py
@@ -13,11 +13,12 @@ from pathlib import Path
 import os
 import environ
 env = environ.Env()
-# reading .env file
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# reading .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 AUTH_USER_MODEL = "users.User"
 
--- alteby/wsgi.py
+++ alteby/wsgi.py
@@ -8,12 +8,16 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 import environ
 import os
+from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 
 env = environ.Env()
-# reading .env file
-environ.Env.read_env()
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# reading .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alteby.settings.' + env('ENVIRONMENT'))
 
