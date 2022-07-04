871316a9bf64f86b5155d48bb862d58b2745871d
muhammed.atif <muhammed.atif@amenli.com>
2022-07-04 02:01:42 +0200

    Del complex query in categories

--- categories/api/views.py
+++ categories/api/views.py
@@ -26,15 +26,9 @@ class CategoryFilter(APIView, PageNumberPagination):
 
     def get(self, request, category_id, format=None):
         try:
-            course_duration_queryset = Unit.objects.filter(course=OuterRef('pk')).annotate(duration_sum=Sum('topics__lectures__duration')).values('duration_sum')[:1]
-
-            courses = Category.objects.get(id=category_id).course_set.prefetch_related('tags', 'categories__course_set', 'privacy__shared_with').select_related('privacy').annotate(
-                units_count=Count('units', distinct=True),
-                lectures_count=Count('units__topics__lectures', distinct=True),
-                course_duration=Coalesce(Subquery(course_duration_queryset), 0, output_field=FloatField()),
-                is_enrolled=Exists(CourseEnrollment.objects.filter(course=OuterRef('pk'), user=self.request.user)),
-                lectures_viewed_count=Count('activity', filter=Q(activity__user=self.request.user), distinct=True)
-            ).all()
+            courses = Category.objects.get(id=category_id).course_set.prefetch_related(
+            'tags', 'categories__course_set', 'privacy__shared_with'
+            ).select_related('privacy').all()
 
         except Category.DoesNotExist:
             return Response(general_utils.error('not_found'), status=status.HTTP_404_NOT_FOUND)
